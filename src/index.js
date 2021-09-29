2e3666d82cc35a37a44c4d7838e3469aad7d20db
harshithmullapudi <harshithmullapudi@gmail.com>
2021-09-29 11:36:47 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const yaml = require('js-yaml');
-
+const core = require("@actions/core");
+const github = require("@actions/github");
+const yaml = require("js-yaml");
 
 async function run() {
   try {
-    const client = github.getOctokit(config['github-token']);
-
+    const client = github.getOctokit(config["github-token"]);
     const app = new App(client);
-
     await app.performActions();
   } catch (err) {
     core.setFailed(err);
@@ -16,30 +13,30 @@ async function run() {
 }
 
 class App {
-  constructor( client) {
+  constructor(client) {
     this.client = client;
   }
 
   async performActions() {
     const payload = github.context.payload;
     console.log(`Hello ${payload}!`);
-    const threadType = payload.issue ? 'issue' : 'pr';
+    const threadType = payload.issue ? "issue" : "pr";
 
     const threadData = payload.issue || payload.pull_request;
 
-    const {owner, repo} = github.context.repo;
-    const issue = {owner, repo, issue_number: threadData.number};
+    const { owner, repo } = github.context.repo;
+    const issue = { owner, repo, issue_number: threadData.number };
     console.log(`Hello ${issue}!`);
-      const newLabels = ["new"]
+    const newLabels = ["new"];
 
-      if (newLabels.length) {
-        core.debug('Labeling');
-        await this.client.rest.issues.addLabels({
-          ...issue,
-          labels: newLabels
-        });
-      }
-    
+    if (newLabels.length) {
+      core.debug("Labeling");
+      await this.client.rest.issues.addLabels({
+        ...issue,
+        labels: newLabels,
+      });
+    }
   }
-
 }
+
+run();
